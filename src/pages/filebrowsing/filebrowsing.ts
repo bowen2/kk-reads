9651b2b0fb8c0279b0a7fa44dfd4311d37796bdf
Zhao Yiheng <yiheng.yh.zhao@gmail.com>
2018-03-12 20:02:38 +0800

    corrected

---
+++ src/pages/filebrowsing/filebrowsing.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-filebrowsing',
+  templateUrl: 'filebrowsing.html'
+})
+export class FilebrowsingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
